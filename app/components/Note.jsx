a06479ec27b353060527e6cf4ce6817507362199
Jared Siirila <siirila@gmail.com>
2017-11-28 22:36:21 +0000

    Added Editing component

--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -1,19 +1,8 @@
 import React from 'react';
 
 
-export default (props) => {
-    var task = props.task;
-    var onDelete = props.onDelete;
-    var id = props.id;
-
-    var handleClick = (evt) => {
-        onDelete(id, evt);
-    }
-
-    return (
-      <div>
-        <span>{task}</span>
-        <button onClick={handleClick}>x</button>
-      </div>
-    )
-}
+export default ({children, ...props}) => (
+  <div {...props}>
+    {children}
+  </div>
+);
