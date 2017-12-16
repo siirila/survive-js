fd1fd3d229b7fb6326b387118f18e2919f90a84b
Jared Siirila <siirila@gmail.com>
2017-12-16 14:21:53 +0000

    Added NoteStore and NoteActions

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -3,27 +3,9 @@ import uuid from 'uuid';
 
 import Notes from './Notes';
 import connect from '../libs/connect';
-
+import NoteActions from '../actions/NoteActions';
 
 class App extends React.Component {
- constructor(props) {
-    super(props);
-    this.addNote = this.addNote.bind(this);
-
-    this.state = {
-      notes: [
-        {
-          id: uuid.v4(),
-          task: 'Learn React'
-        },
-        {
-          id: uuid.v4(),
-          task: 'Do laundry'
-        }
-      ]
-    };
-  }
-
   addNote = () => {
     // It would be possible to write this in an imperative style.
     // I.e., through `this.state.notes.push` and then
@@ -35,11 +17,17 @@ class App extends React.Component {
     // more than make up for it.
     //
     // Libraries, such as Immutable.js, go a notch further.
-    this.setState({
-      notes: this.state.notes.concat([{
-        id: uuid.v4(),
-        task: 'New task'
-      }])
+
+    // this.setState({
+    //   notes: this.state.notes.concat([{
+    //     id: uuid.v4(),
+    //     task: 'New task'
+    //   }])
+    // });
+    
+    this.props.NoteActions.create({
+      id: uuid.v4(),
+      task: 'New Task'
     });
   }
   
@@ -47,39 +35,46 @@ class App extends React.Component {
     // Avoid bubbling to edit
     e.stopPropagation();
 
-    console.log(`The id being deleted is ${id}`);
-    this.setState({
-      notes: this.state.notes.filter(note => note.id !== id)
-    });
+    // console.log(`The id being deleted is ${id}`);
+    // this.setState({
+    //   notes: this.state.notes.filter(note => note.id !== id)
+    // });
+    
+    this.props.NoteActions.delete(id);
+
   }
 
   activateNoteEdit = (id) => {
-    this.setState({
-      notes: this.state.notes.map(note => {
-        if(note.id === id) {
-          note.editing = true;
-        }
+    // this.setState({
+    //   notes: this.state.notes.map(note => {
+    //     if(note.id === id) {
+    //       note.editing = true;
+    //     }
 
-        return note;
-      })
-    });
+    //     return note;
+    //   })
+    // });
+    
+    this.props.NoteActions.update({id, editing: true});
   }
 
   editNote = (id, task) => {
-    this.setState({
-      notes: this.state.notes.map(note => {
-        if(note.id === id) {
-          note.editing = false;
-          note.task = task;
-        }
+    // this.setState({
+    //   notes: this.state.notes.map(note => {
+    //     if(note.id === id) {
+    //       note.editing = false;
+    //       note.task = task;
+    //     }
 
-        return note;
-      })
-    });
+    //     return note;
+    //   })
+    // });
+    
+    this.props.NoteActions.update({id, task, editing: false});
   }
 
   render() {
-    let notes = this.state.notes;
+    let {notes} = this.props;
     return (
       <div>
         {this.props.test}
@@ -95,7 +90,7 @@ class App extends React.Component {
   }
 }
 
-export default connect(() => ({
-  test: 'test'
-}))(App)
+export default connect(({notes}) => ({
+  notes
+}), { NoteActions })(App)
 
